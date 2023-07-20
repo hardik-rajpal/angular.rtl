791827035786682d2948f25c1e12179ca7321790
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-07-21 00:04:27 +0530

    customary deploy script update

--- deploy.ps1
+++ deploy.ps1
@@ -2,8 +2,8 @@ bash -c 'rm -rf .angular node_modules/.cache dist'
 git checkout deploy
 git merge master
 bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.css index.html'    
-ng build --base-href https://webdev-il.github.io/ISID-EPU/;
-bash -c 'mv dist/isid-epu/* .';
+ng build --base-href https://hardik-rajpal.github.io/angular.rtl/;
+bash -c 'mv dist/angular.rtl/* .';
 bash -c 'cp index.html 404.html; rm -rf assets; cp -r src/assets assets';
 git add -A;
 $msg = bash -c "git log -2 --pretty=%B | sed -n '3 p'"
